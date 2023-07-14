d094287eff1ee736a03d5c1b33b9383772f5ac1e
timmySpark <timmyspark1@gmail.com>
2023-07-14 07:43:21 -0700

    [feat] :- getting the questions reqirements right

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,8 +13,9 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		if(alphabet !='e' && alphabet != 'q')
+		if (alphabet != 'e' && alphabet != 'q')
 			putchar(alphabet);
+	putchar('\n');
 
 
 	return (0);
