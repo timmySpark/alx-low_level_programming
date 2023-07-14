8788919f04acd1e0887330b33557b1e5cb98ed5b
timmySpark <timmyspark1@gmail.com>
2023-07-14 07:40:39 -0700

    [feat]:- wrote a program that prints all the letters of the alphabet in lowercase without the letter e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if(alphabet !='e' && alphabet != 'q')
+			putchar(alphabet);
+
+
+	return (0);
+}
