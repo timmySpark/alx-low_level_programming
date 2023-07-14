f84aed3afb7f5070e6c26a588684ab4fb1d5aea3
timmySpark <timmyspark1@gmail.com>
2023-07-14 06:40:09 -0700

    [feat]:- wrote a program that prints the alphabet in lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+		putchar('\n');
+	}
+
+	return (0);
+}
