48983c687c5446e273910fa6cd5b2ebf02410d41
timmySpark <timmyspark1@gmail.com>
2023-07-14 05:54:11 -0700

    [fix]:- tailoring code to pass checker

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 /* more headers goes there */
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
