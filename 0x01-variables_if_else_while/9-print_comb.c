7b1d44eaa725982cbeadb19e0d42c5d1f06eab51
timmySpark <timmyspark1@gmail.com>
2023-07-14 11:12:23 -0700

    [feat]:- a program that prints all the numbers with a comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; ++digit)
+	{
+		putchar(digit);
+		if (digit !='9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
