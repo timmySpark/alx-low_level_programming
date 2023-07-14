2aa8e7d87ccc85393105c15e1d86be9de6759252
timmySpark <timmyspark1@gmail.com>
2023-07-14 10:56:30 -0700

    [feat] :- wrote a program that displays all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char w;
+	int l;
+
+	for (l = '0'; l <= '9'; ++l)
+                putchar(l);
+	for (w = 'a'; w <= 'f'; ++w)
+		putchar(w);
+	putchar('\n');
+
+	return (0);
+}
