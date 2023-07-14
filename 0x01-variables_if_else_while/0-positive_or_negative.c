a45b765548d789d6eaeffaf321d0702365fdfecb
timmySpark <timmyspark1@gmail.com>
2023-07-14 05:59:00 -0700

    [feat] :- adding description for main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-#include <stdio.h>
 
 /**
  * main - Entry point
@@ -9,14 +8,15 @@
  * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/* your code goes there */
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
