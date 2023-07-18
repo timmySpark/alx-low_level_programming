759fa6573d7c88b4a2d97f63795bc38f7714a1d5
timmySpark <timmyspark1@gmail.com>
2023-07-18 11:11:23 -0700

    [feat] :- wrote a program that checks if the user input is greater than, less than or equal to zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - check main
+ * @n: user input
+ * Description: a function that checks if an input is greater, lesser than
+ * or equals Zero
+ * Returns: 1 if n is greater than zero
+ *         -1 if n is less than and zero
+ *          0 if n is equals zero
+ */
+
+
+int print_sign(int n)
+{
+	int num = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		num = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		num = -1;
+	}
+	else
+		_putchar('0');
+		num = 0;
+	return (num);
+}
