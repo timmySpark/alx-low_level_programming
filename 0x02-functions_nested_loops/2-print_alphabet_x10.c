080756f822069cf6bbd01dfb32f26b44cf7d88ba
timmySpark <timmyspark1@gmail.com>
2023-07-18 08:54:03 -0700

    [fix] :- getting the correct output characters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 
 /**
  * print_alphabet_x10 - check Description
- * Description - a function that prints the letters in the alphabet
+ * Description: a function that prints the letters in the alphabet
  * in lowercase ten times, followed by a new line
- * Returns - Nothing
+ * Returns: Nothing
  */
 void print_alphabet_x10(void)
 {
@@ -14,9 +14,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i <= 10; ++i)
 	{
 		for (j = 'a'; j <= 'z'; ++j)
-		{
 			_putchar(j);
-		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
