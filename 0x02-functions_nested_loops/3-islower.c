cf5c5d222486e7a9095df0f5e2c9d34f2dc33230
timmySpark <timmyspark1@gmail.com>
2023-07-18 09:43:00 -0700

    [fix] :- passing betty checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - check main
- * c : an input from the user
+ * _islower - check Description
+ * @c : an input from the user
  * Description : a function that checks for lowercase characters
  * Returns : 1 if c is lowercase and 0 if otherwise
  */
 
-
 int _islower(int c)
 {
-        char alpha;
-        int lower = 0;
+	char alpha;
+	int lower = 0;
 
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-                if (alpha == c)
-                        lower = 1;
-        return (lower);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		if (alpha == c)
+		      lower = 1;
+	return (lower);
 }
 
