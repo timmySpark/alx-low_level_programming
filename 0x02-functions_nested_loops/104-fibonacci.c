6b5c6eeb24dec82cf7e803689864915eaed607f7
timmySpark <timmyspark1@gmail.com>
2023-07-18 18:05:37 -0700

    [feat] :- a program that print the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - a program that displays the first 98 fibonacci numbers
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	unsigned long num_1 = 0, num_2 = 1, sum;
+	int count;
+
+	for (count = 1; count < 98; count++)
+	{
+		sum = num_1 + num_2;
+		num_1 = num_2;
+		num_2 = sum;
+
+		if (count == 98)
+			printf("%lu\n", sum);
+		printf("%lu, ", sum);
+	}
+
+	return (0);
+}
