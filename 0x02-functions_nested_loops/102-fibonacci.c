c90dd9e8f3a6e36fbf9e782ba86e7025e7f9ac83
timmySpark <timmyspark1@gmail.com>
2023-07-18 17:14:34 -0700

    [feat] :- wrote a program that prints 50 fibonaci numbers starting from 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints 50 fibonacci numbers starting with 1 and 2
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int num_1 = 0, num_2 = 1, sum, count;
+
+	for (count = 0; count < 50 ; count++)
+	{
+		sum = num_1 + num_2;
+		num_1 = num_2;
+		num_2 = sum;
+
+		if (count == 49)
+			printf("%d\n", sum);
+		else
+			printf("%d, ", sum);
+	}
+	return (0);
+}
