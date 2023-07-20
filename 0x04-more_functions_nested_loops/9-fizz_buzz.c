69bbc16e268b0f9b5c54e797c6f871320b448cdb
timmySpark <timmyspark1@gmail.com>
2023-07-20 12:37:53 -0700

    [fix] :- passing checker

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100 ; i++)
+	for (i = 1; i < 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -25,7 +25,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
