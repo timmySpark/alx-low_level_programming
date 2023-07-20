10b67679e32f0d6d7e3ee6991928077be939915a
timmySpark <timmyspark1@gmail.com>
2023-07-20 07:19:16 -0700

    [feat] :- wrote a program that multiplies two integer values from the user and displays the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check main
+ * @a: Integer Input
+ * @b: Second Integer Input
+ * Description: a program that multiplies two integers a and b
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
