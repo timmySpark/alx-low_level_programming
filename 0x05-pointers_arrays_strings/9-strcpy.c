a0895f6bdeb90b5621ba9be643ee3b7bb6bb29b7
timmySpark <timmyspark1@gmail.com>
2023-07-24 13:47:48 -0700

    [fix] :- passing a checker

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,12 +13,12 @@
 char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
-	dest[index] = '\0';
 
 	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
+		dest[index] = '\0';
 	}
 
 	return (dest);
