d2acfb93843758b3bd7e14e4517730ac65e0b235
timmySpark <timmyspark1@gmail.com>
2023-07-24 09:33:25 -0700

    [fix] :- tailored my code to fit betty documentation

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strlen - a function that returns the length of a string
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t  _strlen(const char *str);
+size_t _strlen(const char *str);
+
+
+
+
+#endif
