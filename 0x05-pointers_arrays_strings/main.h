901aa364aa6f393ecbc822c8f84d3c64364283fe
timmySpark <timmyspark1@gmail.com>
2023-07-24 13:40:10 -0700

    [feat] :- a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+int _atoi(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
@@ -14,6 +15,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 size_t _strlen(const char *str);
 char *_strcpy(char *dest, const char *src);
+
 
 
 
