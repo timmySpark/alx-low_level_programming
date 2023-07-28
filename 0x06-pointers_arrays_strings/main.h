fb5072d91d566743b7f097189c76bf1c54268bb9
timmySpark <timmyspark1@gmail.com>
2023-07-27 18:26:44 -0700

    [feat] :- wrote a function that adds two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 char *leet(char *str);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _strcmp(char *s1, char *s2);
 int _putchar(char n);
 void reverse_array(int *a, int n);
