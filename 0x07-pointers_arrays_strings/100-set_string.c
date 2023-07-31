1d16ed0bb54782a52ecca2fe2ce8a468d6487b76
timmySpark <timmyspark1@gmail.com>
2023-07-31 15:47:50 -0700

    [typo] :- fixed the mistake in the dscription of the set_string file

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
 /**
  * set_string - check main
  * @s: the pointer to be stored
- * *to: where the pointer is to be stored
+ * @to: where the pointer is to be stored
  * Description -  a function that sets the value of a pointer
  *                                                     to a char.
  */
