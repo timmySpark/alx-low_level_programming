791b0cf8618b48efc4dcfedcd94fecc633e4fad6
timmySpark <timmyspark1@gmail.com>
2023-07-31 15:46:08 -0700

    [feat] :- wrote a function that sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - check main
+ * @s: the pointer to be stored
+ * *to: where the pointer is to be stored
+ * Description -  a function that sets the value of a pointer
+ *                                                     to a char.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
