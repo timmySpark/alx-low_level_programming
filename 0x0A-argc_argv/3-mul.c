e3972f1b3f2a1e8aafb845d9c2fe0e5363c09069
timmySpark <timmyspark1@gmail.com>
2023-08-04 10:14:54 -0700

    [feat] :- wrote a program that multiplies two numbers from th argument passed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints it's name followed by a new line
+ * @argc: Number of command line arguments supplied to the program
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mult;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mult = num1 * num2;
+
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
