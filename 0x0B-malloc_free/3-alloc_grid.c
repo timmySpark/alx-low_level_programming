5107ed5326ab149afc322e0c43b1f3fbf2f6e998
timmySpark <timmyspark1@gmail.com>
2023-08-08 10:34:10 -0700

    [fix] :- fixing betty-style errors and meeting other requirements of the checker

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,21 +16,23 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
+
 	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
-        {
+	{
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
-                {
+		{
 			for (j = 0; j < i; j++)
 				free(grid[j]);
-		       	return (NULL);
+
+			free(grid);
+			return (NULL);
 		}
 
 
