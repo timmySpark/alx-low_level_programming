efef1eceb95351855c1384fe9526d77e4e9f9b8f
timmySpark <timmyspark1@gmail.com>
2023-08-14 12:23:02 -0700

    [feat] :- getting the right way to  comment

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/**
+/*
  * File: 0-object_like_macro.h
  * Description: A header file that defines a macro named SIZE as 
  *                                      an abbreviation for the token 1024.
