3191ef3a1d8f3921340ad8094a845009f506fa05
timmySpark <timmyspark1@gmail.com>
2023-08-16 12:00:51 -0700

    [feat] :- wrote a funtion that executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array_interator - Executes a function given as a parameter on each
+ *                                        element of an array.
+ * @array: array to be used
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+	int res;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		res = array[i];
+		action(res);
+
+	}
+}
