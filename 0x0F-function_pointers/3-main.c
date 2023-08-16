32ce771f12901bb6f17b999642fafcd868c13141
timmySpark <timmyspark1@gmail.com>
2023-08-16 13:48:14 -0700

    [fix] :- trying to pass requirement checks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,13 +28,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
