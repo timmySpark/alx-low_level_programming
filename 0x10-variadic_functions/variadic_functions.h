fd72f4a2b2fb3327f21533a751d76b8c02306d80
timmySpark <timmyspark1@gmail.com>
2023-08-17 11:33:58 -0700

    [feat] :- added a struct print to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,15 +1,33 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
 
 int _putchar(int c);
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+
+/**
+ * struct print - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @display: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
+typedef struct print
+{
+	char *symbol;
+	void (*display)(va_list arg);
+
+} print_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-
 
 
 
