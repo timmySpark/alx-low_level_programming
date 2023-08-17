0fa281b758ac758fb3dd7ed58266a7c0b7709e01
timmySpark <timmyspark1@gmail.com>
2023-08-17 12:27:44 -0700

    [feat] :- passing requirement checks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str =  va_arg(strings, char *);
+		str =  va_arg(strings, char*);
 
 		if (str == NULL)
 			printf("(nil)");
