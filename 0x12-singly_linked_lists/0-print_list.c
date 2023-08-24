62a5a014b9b957462dbec126d6baa34e35dcec66
timmySpark <timmyspark1@gmail.com>
2023-08-24 11:45:10 -0700

    [feat]:- passing betty checks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list - print all the elements of a list_t list
+ * size_t print_list - print all the elements of a list_t list
  * @h: list_t list
  *
  * Return: Number of nodes in h
