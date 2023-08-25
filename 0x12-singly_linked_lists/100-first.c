3136414338b2ddf07e30aa294fc1baa7c7327f9e
timmySpark <timmyspark1@gmail.com>
2023-08-24 17:13:45 -0700

    [feat]:- tried using __attribute__ in another way

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 
+void __attribute__((constructor)) tortoise_and_hare(void);
+
+
 /**
  * _tortoise_and_hare - print a message
  */
 
-void _tortoise_and_hare(void)
+void tortoise_and_hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,13 +21,14 @@ typedef struct list_s
 
 
 int _putchar(char c);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
-
+void __attribute__ ((constructor)) tortoise_and_hare(void);
 
 
 
