530c198405ca9943df78508b30e6c995f7118ea7
timmySpark <timmyspark1@gmail.com>
2023-08-24 17:03:06 -0700

    [feat] :- print a message before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _tortoise_and_hare - print a message
+ */
+
+void _tortoise_and_hare()
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+
+}
