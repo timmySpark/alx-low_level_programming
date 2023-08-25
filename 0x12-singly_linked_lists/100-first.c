ea4d7b7bbfa26b6cce45202ec644bd7e8b00a780
timmySpark <timmyspark1@gmail.com>
2023-08-24 17:07:38 -0700

    [feat]: passing betty checks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * _tortoise_and_hare - print a message
  */
 
-void _tortoise_and_hare()
+void _tortoise_and_hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
