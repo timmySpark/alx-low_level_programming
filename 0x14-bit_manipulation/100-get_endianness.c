8b5d37da5212ef7dd9a042bdba3645609454c82a
timmySpark <timmyspark1@gmail.com>
2023-08-31 15:47:44 -0700

    [feat] :- checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianess
+ *
+ * Return: If big-endian - 0
+ *            little-endian - 1
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+
+}
