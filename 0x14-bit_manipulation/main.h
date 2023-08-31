c0c2a42f0d8a729c261312e2d4f0a1eb5bd7ad79
timmySpark <timmyspark1@gmail.com>
2023-08-31 15:53:10 -0700

    [feat] :- added prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 int _putchar(char c);
+int get_endianness(void);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
