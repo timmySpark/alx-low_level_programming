6212df3879ad10114d8f0fb58d65ebc0fdf28b05
timmySpark <timmyspark1@gmail.com>
2023-11-03 16:09:20 -0700

    [feat] :- working on hash tabkles

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ *hash_table_create - function that creates a hash table.
+ *@size: size of array.
+ *
+ *
+ *Return: NULL if something goes wrong
+	otherwise -  New table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newTable;
+	unsigned long int i;
+
+	newTable = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!newTable)
+		return (NULL);
+	newTable->size = size;
+	newTable->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (!newTable->array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		newTable->array[i] = NULL;
+
+	return (newTable);
+}
